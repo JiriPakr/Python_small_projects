045ea7c3a9be81a61871b39c28bf279ede416e94
Jiri Pakr <jiripakr@gmail.com>
2023-06-22 14:10:16 +0200

    small fixes

--- Challanges/Easy/704.Binary_Search.py
+++ Challanges/Easy/704.Binary_Search.py
@@ -44,7 +44,7 @@ def search1(nums, target: int) -> int:
 def search2(nums, target: int) -> int:
     l,r = 0, len(nums) - 1
     while l <= r:
-        m = (l + r) // 2
+        m = l + (r-l) // 2
         if nums[m] > target:
             r = m - 1
         elif nums[m] < target:
