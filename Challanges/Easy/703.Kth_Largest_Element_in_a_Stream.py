f9f4455ba4667c191bc8291afa79a01ed7152843
Jiri Pakr <jiripakr@gmail.com>
2023-07-21 10:18:48 +0200

    adding 703 v2

--- Challanges/Easy/703.Kth_Largest_Element_in_a_Stream.py
+++ Challanges/Easy/703.Kth_Largest_Element_in_a_Stream.py
@@ -33,15 +33,22 @@ Constraints:
     At most 104 calls will be made to add.
     It is guaranteed that there will be at least k elements in the array when you search for the kth element.
 """
+import heapq
 ########################################################################
 class KthLargest:
 
-    def __init__(self, k: int, nums: List[int]):
+    def __init__(self, k: int, nums):
+      # minHeap with K largest integers
+      self.minHeap, self.k = nums, k
+      heapq.heapify(self.minHeap)
+      while len(self.minHeap) > k:
+         heapq.heappop(self.minHeap)
         
-
     def add(self, val: int) -> int:
-        
-
+      heapq.heappush(self.minHeap, val)
+      if len(self.minHeap) > self.k:
+        heapq.heappop(self.minHeap)
+      return self.minHeap[0]
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
